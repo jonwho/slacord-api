be3cccd4d2dd1fd65597f0bedac2b40eb122e14d
Jon Ho <jonwho.contact@gmail.com>
2017-07-26 15:37:00 -0700

    show action ok

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,14 +2,19 @@ class Api::V1::UsersController < Api::V1::BaseController
   def create
     user = User.create(user_params)
     if user.save
-      render json: User.find(user.id)
+      render json: User.find(user.id), status: :created
     else
       render json: { errors: user.errors.messages }, status: :bad_request
     end
   end
 
   def show
-    respond_with User.find(params[:id])
+    user = User.find_by(id: params[:id])
+    if user
+      render json: user
+    else
+      render json: { errors: ["could not find user with id #{params[:id]}"] }, status: :not_found
+    end
   end
 
   private
--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -16,8 +16,12 @@ RSpec.describe Api::V1::UsersController, type: :request do
         post api_v1_users_path, params: params
       end.to change{User.count}.by(1)
 
+      expect(response.status).to be 201
+
       json_response = JSON.parse(response.body)
 
+      # TODO: test json response better
+      # need to ensure only returning serialized data
       expect(json_response).to include('auth_token')
       expect(json_response).to include('email')
       expect(json_response).to include('username')
@@ -39,6 +43,37 @@ RSpec.describe Api::V1::UsersController, type: :request do
 
       json_response = JSON.parse(response.body)
 
+      # TODO: test json response better
+      # need to ensure only returning serialized data
+      expect(json_response).to include('errors')
+    end
+  end
+
+    let(:user) { FactoryGirl.create(:user) }
+  describe 'GET #show' do
+    it 'retrieves user info with given id' do
+      get api_v1_user_path(user.id)
+
+      expect(response.status).to be 200
+
+      json_response = JSON.parse(response.body)
+
+      # TODO: test json response better
+      # need to ensure only returning serialized data
+      expect(json_response).to include('auth_token')
+      expect(json_response).to include('email')
+      expect(json_response).to include('username')
+    end
+
+    it 'returns 404 for user not found' do
+      get api_v1_user_path(404)
+
+      expect(response.status).to be 404
+
+      json_response = JSON.parse(response.body)
+
+      # TODO: test json response better
+      # need to ensure only returning serialized data
       expect(json_response).to include('errors')
     end
   end
