caefcf2b7076457c81b757504209cc1e3eb226fb
Jon Ho <jonwho.contact@gmail.com>
2017-07-26 16:01:21 -0700

    awesome user only returns serialized data for json responses now

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'rack-cors'
 # use jwt pronounced 'jot' for token auth
 gem 'jwt'
 
+gem 'active_model_serializers'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726213621) do
+ActiveRecord::Schema.define(version: 20170726225854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
